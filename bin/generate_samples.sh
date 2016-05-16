2cf5c0b102d79e87349c5ba0582a26481edde354
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-05-16 15:00:11 +0100

    Updated license text

--- bin/generate_samples.sh
+++ bin/generate_samples.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-samples=100
+# This file is part of SB pipe.
+#
+# SB pipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SB pipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SB pipe.  If not, see <http://www.gnu.org/licenses/>.
+
+samples=1000
 
 Rscript ${SB_PIPE_LIB}/R/tc_data_generator.R generate_samples_dataset.csv generate_samples_dataset_output.csv ${samples} true
 
