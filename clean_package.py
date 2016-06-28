3486f4ad4f6f4d9b2baba52565e32edeef39ec0e
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-06-28 11:42:47 +0100

    project renamed from SB_pipe (or SB pipe) to sb_pipe. This makes the project name consistent with the python project name.

--- clean_package.py
+++ clean_package.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This file is part of SB pipe.
+# This file is part of sb_pipe.
 #
-# SB pipe is free software: you can redistribute it and/or modify
+# sb_pipe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SB pipe is distributed in the hope that it will be useful,
+# sb_pipe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with SB pipe.  If not, see <http://www.gnu.org/licenses/>.
+# along with sb_pipe.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 # Object: clean the package
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This file is part of SB pipe.
+# This file is part of sb_pipe.
 #
-# SB pipe is free software: you can redistribute it and/or modify
+# sb_pipe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SB pipe is distributed in the hope that it will be useful,
+# sb_pipe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with SB pipe.  If not, see <http://www.gnu.org/licenses/>.
+# along with sb_pipe.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 #
@@ -51,8 +51,8 @@ setup(
   requires=['pp','numpy','scipy'],
   package_data={'sb_pipe': ['doc/*.pdf','doc/*.html','*.tgz']},
   author_email = 'piero.dallepezze@babraham.ac.uk',
-  url = 'https://github.com/pdp10/SB_pipe',
-  download_url = 'http://pdp10.github.io/SB_pipe/',
+  url = 'https://github.com/pdp10/sb_pipe',
+  download_url = 'http://pdp10.github.io/sb_pipe/',
   keywords = ['systems biology','mathematical modelling','copasi','pipeline'],
   include_package_data=False,
   license='GNU GPL v3',
