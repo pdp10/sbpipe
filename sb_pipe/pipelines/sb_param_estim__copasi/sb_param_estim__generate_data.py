e7f738bdb8b0b0f1c272104b8a7ac488b0ead186
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-07-11 10:54:25 +0100

    testing qsubProc.wait() at the end for waiting all processes

--- sb_pipe/pipelines/sb_param_estim__copasi/sb_param_estim__generate_data.py
+++ sb_pipe/pipelines/sb_param_estim__copasi/sb_param_estim__generate_data.py
@@ -58,7 +58,7 @@ def runCopasiSGE(models_dir, model, outDir, errDir, nfits):
   # Check here when these jobs are finished before proceeding
   qsubCMD = ["qsub", "-hold_jid", jobs[:-1]]
   echoProc = Popen(echoSleep, stdout=PIPE)
-  qsubProc = Popen(qsubCMD, stdin=echoProc.stdout, stdout=PIPE)
+  qsubProc = Popen(qsubCMD, stdin=echoProc.stdout)
   qsubProc.wait()
 
 
