9570237d9f07df377f2e9bf772e2c5642d1ee6a5
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-07-17 12:43:30 +0100

    Changed license from GPLv3 to LGPLv3

--- sb_pipe/pipelines/sb_param_estim__pw/param_estim__pw_analyses_full_repository.m
+++ sb_pipe/pipelines/sb_param_estim__pw/param_estim__pw_analyses_full_repository.m
@@ -1,16 +1,16 @@
 % This file is part of sb_pipe.
 %
 % sb_pipe is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
+% it under the terms of the GNU Lesser General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
 % sb_pipe is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
+% GNU Lesser General Public License for more details.
 %
-% You should have received a copy of the GNU General Public License
+% You should have received a copy of the GNU Lesser General Public License
 % along with sb_pipe.  If not, see <http://www.gnu.org/licenses/>.
 %
 %
