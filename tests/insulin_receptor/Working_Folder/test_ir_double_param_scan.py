050f06a8a917811c6d6352f25b7356b29f0c896f
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-08-10 17:19:53 +0100

    Added test files for double parameter scan

---
+++ tests/insulin_receptor/Working_Folder/test_ir_double_param_scan.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of sb_pipe.
+#
+# sb_pipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sb_pipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sb_pipe.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Object: run a list of tests for the insulin receptor model.
+#
+# $Revision: 3.0 $
+# $Author: Piero Dalle Pezze $
+# $Date: 2016-01-21 10:36:32 $
+
+
+import os
+import sys
+from distutils.dir_util import copy_tree
+
+SB_PIPE = os.environ["SB_PIPE"]
+sys.path.append(os.path.join(SB_PIPE,'sb_pipe'))
+
+import run_sb_pipe
+
+import unittest
+
+
+"""Unit test for Insulin Receptor"""
+
+class TestIRDoubleParamScan(unittest.TestCase):
+  """
+  A collection of tests for this example.
+  """
+  def test_double_param_scan_inhib_only(self):    
+    """model double param scan - inhibition only"""
+    self.assertEqual(run_sb_pipe.main(["run_sb_pipe", "--double-param-scan", "ir_model_insulin_ir_beta_dbl_inhib.conf"]), 0)     
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    
+    
