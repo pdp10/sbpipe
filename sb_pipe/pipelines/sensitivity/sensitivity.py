134089bde443626104da8b933cf67673f5fab908
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-08-19 09:18:42 +0100

    #111 pipeline sensitivity is unified

--- sb_pipe/pipelines/sensitivity/sensitivity.py
+++ sb_pipe/pipelines/sensitivity/sensitivity.py
@@ -36,14 +36,15 @@ import subprocess
 import logging
 logger = logging.getLogger('sbpipe')
 
-import sensitivity__generate_data
-import sensitivity__analyse_data
-import sensitivity__generate_report
 
 SB_PIPE = os.environ["SB_PIPE"]
+
+from sb_config import get_copasi, which
+
 sys.path.append(os.path.join(SB_PIPE, "sb_pipe", "utils", "python"))
 from config_parser import config_parser
-
+from io_util_functions import refresh_directory
+from latex_reports import latex_report_simulate
 
 
 """
@@ -52,86 +53,169 @@ model sensitivity analysis using Copasi
 """
 
 def main(config_file):
-  """
-  Execute and collect results for model sensitivity using Copasi
-  Keyword arguments:
-      config_file -- the file containing the model configuration, usually in working_folder (e.g. model.conf)
-  """
+    """
+    Execute and collect results for model sensitivity using Copasi
+    Keyword arguments:
+	config_file -- the file containing the model configuration, usually in working_folder (e.g. model.conf)
+    """
 
-  logger.info("Reading file " + config_file + " : \n")
-  
-  # Initialises the variables for this pipeline
-  try:
-    (generate_data, analyse_data, generate_report,
-      project_dir, model) = config_parser(config_file, "sensitivity")  
-  except Exception as e:
-    logger.error(e.message)
-    import traceback
-    logger.debug(traceback.format_exc())    
-    return 2
-  
-  
-  # INTERNAL VARIABLES
-  # The folder containing the models
-  models_folder="Models"
-  # The working folder containing the results
-  working_folder="Working_Folder"  
-  # The folder containing the sensitivity analysis results
-  sensitivities_dir="sensitivities"
-  
-  models_dir = os.path.join(project_dir, models_folder)
-  outputdir = os.path.join(project_dir, working_folder, model[:-4], sensitivities_dir)
+    logger.info("Reading file " + config_file + " : \n")
+    
+    # Initialises the variables for this pipeline
+    try:
+	(generate_data, analyse_data, generate_report,
+	  project_dir, model) = config_parser(config_file, "sensitivity")  
+    except Exception as e:
+	logger.error(e.message)
+	import traceback
+	logger.debug(traceback.format_exc())    
+	return 2
+    
+    
+    # INTERNAL VARIABLES
+    # The folder containing the models
+    models_folder="Models"
+    # The working folder containing the results
+    working_folder="Working_Folder"  
+    # The folder containing the sensitivity analysis results
+    sensitivities_dir="sensitivities"
+    
+    models_dir = os.path.join(project_dir, models_folder)
+    outputdir = os.path.join(project_dir, working_folder, model[:-4], sensitivities_dir)
 
 
-  # Get the pipeline start time
-  start = datetime.datetime.now().replace(microsecond=0)
+    # Get the pipeline start time
+    start = datetime.datetime.now().replace(microsecond=0)
 
+	
+
+    logger.info("\n")
+    logger.info("Processing model " + model)
+    logger.info("#############################################################")
+    logger.info("")
+
+
+    # preprocessing
+    # remove the folder the previous results if any
+  #   filesToDelete = glob.glob(os.path.join(sensitivities_dir, "*.png"))
+  #   for f in filesToDelete:
+  #     os.remove(f)
+    if not os.path.exists(outputdir):
+	os.mkdir(outputdir)
+
+
+
+    if generate_data == True:
+	logger.info("\n")
+	logger.info("Data generation:")
+	logger.info("################")
+	fun_generate_data(model, models_dir, outputdir) 
+
+
+    if analyse_data == True:
+	logger.info("\n")
+	logger.info("Data analysis:")
+	logger.info("##############")
+	fun_analyse_data(outputdir)  
+
+
+    if generate_report == True:
+	logger.info("\n")
+	logger.info("Report generation:")
+	logger.info("##################")
+	fun_generate_report()     
+
+
+    # Print the pipeline elapsed time
+    end = datetime.datetime.now().replace(microsecond=0)
+    logger.info("\n\nPipeline elapsed time (using Python datetime): " + str(end-start)) 
+
+
+    if len(glob.glob(os.path.join(outputdir, '*.csv'))) > 0:
+	return 0
+    return 1
+    
+
+
+
+
+
+
+# Input parameters
+# model, inputdir, outputdir
+def fun_generate_data(model, inputdir, outputdir):
+
+    if not os.path.isfile(os.path.join(inputdir,model)):
+	logger.error(os.path.join(inputdir, model) + " does not exist.") 
+	return  
+
+    # folder preparation
+    refresh_directory(outputdir, model[:-4])
+
+    # execute runs simulations.
+    logger.info("Sensitivity analysis for " + model)
+    
+    # run copasi
+    copasi = get_copasi()
+    if copasi == None:
+	logger.error("CopasiSE not found! Please check that CopasiSE is installed and in the PATH environmental variable.")
+	return  
+    
+    command = [copasi, os.path.join(inputdir, model[:-4]+".cps")]
+
+    p = subprocess.Popen(command)
+    p.wait()
+  
+    # move the output file
+    move(os.path.join(model[:-4]+".csv"), outputdir)
+  
+  
+  
+  
+
+
+
+# Input parameters
+# outputdir
+def fun_analyse_data(outputdir):
+    process = subprocess.Popen(['Rscript', os.path.join(SB_PIPE,'sb_pipe','pipelines','sensitivity','plot_sensitivity.r'), outputdir])
+    process.wait()
+
+
+
+
+
+
+
+# INITIALIZATION
+# model_noext: read the model_noext
+# outputdir: read the outputdir  
+# sim_plots_folder: the directory containing the time courses results combined with experimental data  
+def fun_generate_report(model_noext, outputdir, sim_plots_folder):
+    
+    if not os.path.exists(os.path.join(outputdir, sim_plots_folder)): 
+	logger.error("input_dir " + os.path.join(outputdir, sim_plots_folder) + " does not exist. Analyse the data first.");
+	return    
       
+    logger.info("Generating a LaTeX report")
+    filename_prefix="report__sensitivity_"
+    latex_report_simulate(outputdir, sim_plots_folder, model_noext, filename_prefix)
+    
+    pdflatex = which("pdflatex")
+    if pdflatex == None:
+	logger.error("pdflatex not found! pdflatex must be installed for pdf reports.")
+	return
+      
+    logger.info("Generating PDF report")  
+    currdir=os.getcwd()
+    os.chdir(outputdir)
 
-  logger.info("\n")
-  logger.info("Processing model " + model)
-  logger.info("#############################################################")
-  logger.info("")
-
-
-  # preprocessing
-  # remove the folder the previous results if any
-#   filesToDelete = glob.glob(os.path.join(sensitivities_dir, "*.png"))
-#   for f in filesToDelete:
-#     os.remove(f)
-  if not os.path.exists(outputdir):
-    os.mkdir(outputdir)
-
-
-
-  if generate_data == True:
-    logger.info("\n")
-    logger.info("Data generation:")
-    logger.info("################")
-    sensitivity__generate_data.main(model, models_dir, outputdir) 
-
-
-  if analyse_data == True:
-    logger.info("\n")
-    logger.info("Data analysis:")
-    logger.info("##############")
-    sensitivity__analyse_data.main(outputdir)  
-
-
-  if generate_report == True:
-    logger.info("\n")
-    logger.info("Report generation:")
-    logger.info("##################")
-    sensitivity__generate_report.main()     
-
-
-  # Print the pipeline elapsed time
-  end = datetime.datetime.now().replace(microsecond=0)
-  logger.info("\n\nPipeline elapsed time (using Python datetime): " + str(end-start)) 
-
-
-  if len(glob.glob(os.path.join(outputdir, '*.csv'))) > 0:
-      return 0
-  return 1
+    logger.info(pdflatex + " -halt-on-error " + filename_prefix + model_noext + ".tex ... ") 
+    p1 = subprocess.Popen([pdflatex, "-halt-on-error", filename_prefix + model_noext + ".tex"], stdout=subprocess.PIPE)
+    p1.communicate()[0]
+    p1 = subprocess.Popen([pdflatex, "-halt-on-error", filename_prefix + model_noext + ".tex"], stdout=subprocess.PIPE)
+    p1.communicate()[0]
+    
+    os.chdir(currdir)
     
     
