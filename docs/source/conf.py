9ef08bc5898d8b56a16ff7e36ebacf34c786fe0c
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-10-05 16:36:34 +0100

    added copyrights and updated author list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SB pipe'
-copyright = u'2016, Piero Dalle Pezze'
+copyright = u'2016, Piero Dalle Pezze and Nicolas Le Novère'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -239,7 +239,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'sb_pipe.tex', u'SB pipe documentation',
-   u'Piero Dalle Pezze', 'manual'),
+   u'Piero Dalle Pezze and Nicolas Le Novère', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -269,7 +269,7 @@ latex_show_urls = True
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'sb_pipe', u'SB pipe documentation',
-     [u'Piero Dalle Pezze'], 1)
+     [u'Piero Dalle Pezze and Nicolas Le Novère'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -283,7 +283,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'sb_pipe', u'SB pipe documentation',
-   u'Piero Dalle Pezze', 'sb_pipe', 'One line description of project.',
+   u'Piero Dalle Pezze and Nicolas Le Novère', 'sb_pipe', 'One line description of project.',
    'Miscellaneous'),
 ]
 
