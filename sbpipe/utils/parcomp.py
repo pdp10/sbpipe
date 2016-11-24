68bd1f83c7a93ad944f07b3cb0585c01e51e233c
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-11-24 12:09:09 +0000

    added `-V` to qsub -sync

--- sbpipe/utils/parcomp.py
+++ sbpipe/utils/parcomp.py
@@ -206,7 +206,7 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs):
         logger.debug(qsub_cmd)
     # Check here when these jobs are finished before proceeding
     # don't add names for output and error files as they can generate errors..
-    qsub_cmd = ["qsub", "-sync", "y", "-b", "y", "-hold_jid",
+    qsub_cmd = ["qsub", "-V", ""-sync", "y", "-b", "y", "-hold_jid",
                 jobs[:-1], "sleep", "1"]  # , "-o", os.path.join(outDir, "wait"), "-e", os.path.join(errDir, "wait")]
     echo_proc = subprocess.Popen(echo_sleep, stdout=subprocess.PIPE)
     qsub_proc = subprocess.Popen(qsub_cmd, stdin=echo_proc.stdout, stdout=subprocess.PIPE)
