65fb04f963fcb5642d2aea6128a952c7c288bea6
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-11-25 11:38:59 +0000

    Improving wait command.

--- sbpipe/utils/parcomp.py
+++ sbpipe/utils/parcomp.py
@@ -205,7 +205,7 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs):
         logger.debug(qsub_cmd)
     # Check here when these jobs are finished before proceeding
     # don't add names for output and error files as they can generate errors..
-    qsub_cmd = ["qsub", "-sync", "y", "-b", "y", "-hold_jid", jobs[:-1], "sleep", "1"] # "-o", "/dev/null", "-e", "/dev/null",
+    qsub_cmd = ["qsub", "-sync", "y", "-b", "y", "-o", "/dev/null", "-e", "/dev/null", "-hold_jid", jobs[:-1], "sleep", "1"]
     qsub_proc = subprocess.Popen(qsub_cmd, stdout=subprocess.PIPE)
     qsub_proc.communicate()[0]
     logger.debug(qsub_cmd)
