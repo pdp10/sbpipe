c19e10806d74ab6c05985d483ea3af466b1bb67f
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-11-25 10:36:13 +0000

    Corrected wait command.

--- sbpipe/utils/parcomp.py
+++ sbpipe/utils/parcomp.py
@@ -201,12 +201,18 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs):
     for i in xrange(1, runs + 1):
         # Now the same with qsub
         jobs = "j" + str(i) + "," + jobs
-        qsub_cmd = ["qsub", "-cwd", "-V", "-N", "j" + str(i), "-o", os.path.join(out_dir, "j" + str(i)), "-e", os.path.join(err_dir, "j" + str(i)), "-b", "y", cmd.replace(cmd_iter_substr, str(i))]
+        qsub_cmd = ["qsub", "-cwd", "-V", "-N", "j" + str(i),
+                    "-o", os.path.join(out_dir, "j" + str(i)),
+                    "-e", os.path.join(err_dir, "j" + str(i)),
+                    "-b", "y", cmd.replace(cmd_iter_substr, str(i))]
         qsub_proc = subprocess.Popen(qsub_cmd, stdout=subprocess.PIPE)
         logger.debug(qsub_cmd)
     # Check here when these jobs are finished before proceeding
-    #  don't add names for output and error files as they can generate errors..
-    qsub_cmd = ["qsub", "-sync", "y", "-o", os.path.join(out_dir, "wait"), "-e", os.path.join(err_dir, "wait")], "-b", "y", "-hold_jid", jobs[:-1], "sleep", "1"]
+    # don't add names for output and error files as they can generate errors..
+    qsub_cmd = ["qsub", "-sync", "y",
+                "-o", os.path.join(out_dir, "wait"),
+                "-e", os.path.join(err_dir, "wait"),
+                "-hold_jid", jobs[:-1], "-b", "y", "sleep", "1"]
     echo_proc = subprocess.Popen(echo_sleep, stdout=subprocess.PIPE)
     qsub_proc = subprocess.Popen(qsub_cmd, stdin=echo_proc.stdout, stdout=subprocess.PIPE)
     qsub_proc.communicate()[0]
