746f52ed5ca59ea666bfc48db97c28bf976f3507
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-12-06 22:29:46 +0000

    added test cases for R models of stochastic differential equations (SDE)

---
+++ tests/rscript/Models/sde_cox_ingersoll_ross_process.r
@@ -0,0 +1,36 @@
+
+
+# Model from https://cran.r-project.org/web/packages/sde/sde.pdf
+
+# import sde package
+# sde and its dependencies must be installed.
+if(!require(sde)){
+    install.packages('sde')
+    library(sde)
+}
+
+
+
+# Model definition
+# ---------------------------------------------
+# set.seed()
+# Cox-Ingersoll-Ross process
+# dXt = (6-3*Xt)*dt + 2*sqrt(Xt)*dWt
+
+d <- expression( 6-3*x )
+s <- expression( 2*sqrt(x) )
+X0 <- 10
+
+delta <- 1/50
+N <- 500
+time <- seq(0, N*delta, by=delta)
+
+Cox_Ingersoll_Ross <- sde.sim(delta=delta, X0=X0, N=N, drift=d, sigma=s)
+
+out <- data.frame(time, Cox_Ingersoll_Ross)
+# ---------------------------------------------
+
+# Write the output. The output file must be the model name with csv or txt extension.
+# Fields must be separated by TAB, and row names must be discarded.
+write.table(out, file="sde_cox_ingersoll_ross_process.csv", sep="\t", row.names=FALSE)
+
---
+++ tests/rscript/Models/sde_periodic_drift.r
@@ -0,0 +1,35 @@
+
+
+# Model from https://cran.r-project.org/web/packages/sde/sde.pdf
+
+# import sde package
+# sde and its dependencies must be installed.
+if(!require(sde)){
+    install.packages('sde')
+    library(sde)
+}
+
+
+
+# Model definition
+# ---------------------------------------------
+# set.seed()
+d <- expression(sin(x))
+d.x <- expression(cos(x))
+A <- function(x) 1-cos(x)
+
+X0 <- 0
+delta <- 1/20
+N <- 500
+time <- seq(X0, N*delta, by=delta)
+
+# EA = exact method
+periodic_drift <- sde.sim(method="EA", delta=delta, X0=X0, N=N, drift=d, drift.x=d.x, A=A)
+
+out <- data.frame(time, periodic_drift)
+# ---------------------------------------------
+
+# Write the output. The output file must be the model name with csv or txt extension.
+# Fields must be separated by TAB, and row names must be discarded.
+write.table(out, file="sde_periodic_drift.csv", sep="\t", row.names=FALSE)
+
