0e080041299393f4f7f29f5c74b96dec5f022f9b
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-12-08 17:23:00 +0000

    added Python script to automatically clone my Java model via git, replace the main class in the pom.xml file, and finally recompile it with maven

--- tests/java_models/Models/README.txt
+++ tests/java_models/Models/README.txt
@@ -1,3 +1,22 @@
 Model from https://github.com/pdp10/simqueue
 Author: Piero Dalle Pezze
 License: MIT
+Year: 2005
+
+This is a queue simulator based on stochastic time events.
+It aims to simulate a real queue, like a queue at the post office,
+where clients arrive in a random order. When a client arrives, s/he is
+the last one who will be served. (FIFO = First In First Out). In particular
+there are two stochastic factors:
+    1) the time of arrive,
+    2) the time of service.
+Meanwhile, clients arrive and others leave.
+This simulation shows when a person arrives, is ready to be served, and finally leaves.
+
+
+
+To generate the jar file, run the python script:
+python get_jar_model.py
+
+NOTE: `maven` and `git` packages are required.
+
---
+++ tests/java_models/Models/get_jar_model.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of sbpipe.
+#
+# sbpipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sbpipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sbpipe.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+import os
+import subprocess
+from shutil import copy2, rmtree
+
+SBPIPE = os.environ["SBPIPE"]
+sys.path.append(os.path.join(SBPIPE))
+from sbpipe.sb_config import which
+from sbpipe.utils.io import replace_str_in_file
+
+
+def get_simqueue():
+    """
+    Retrieve simqueue
+    """
+    subprocess.Popen(['git','clone', 'https://github.com/pdp10/simqueue.git']).communicate()[0]
+    print("\n")
+
+def compile_simqueue():
+    """
+    Compile simqueue
+    """
+    # need to change the main class name
+    print("editing simqueue pom.xml file\n")
+    replace_str_in_file(os.path.join('.', 'simqueue','pom.xml'),'org.simqueue.Main','org.simqueue.MainTest')
+    # let's change directory
+    # remember our original working directory
+    orig_wd = os.getcwd()
+    os.chdir(os.path.join('.', 'simqueue'))
+    subprocess.Popen(['mvn', 'package']).communicate()[0]
+    print("\n")
+    os.chdir(orig_wd)
+    copy2(os.path.join('.', 'simqueue', 'target', 'simqueue-devel-jar-with-dependencies.jar'), orig_wd)
+
+def cleanup():
+    """
+    Cleanup simqueue
+    """
+    print('cleanup!')
+    rmtree(os.path.join('.', 'simqueue'), ignore_errors=True)
+
+
+def main(args=None):
+    if which('git') is None:
+        print('Error: git was not found. Quit')
+        return
+    if which('mvn') is None:
+        print('Error: mvn was not found. Quit')
+        return
+    get_simqueue()
+    compile_simqueue()
+    cleanup()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
