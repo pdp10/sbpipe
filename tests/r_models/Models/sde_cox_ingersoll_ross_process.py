33d25aee351645e512eb98800e8401376bff462d
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2017-01-28 14:30:13 +0000

    #173 Running Java, R, and Octave testing models using Python wrappers.

---
+++ tests/r_models/Models/sde_cox_ingersoll_ross_process.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import subprocess
+import shlex
+
+# This is a Python wrapper used to run an R model.
+# The R model receives the report_filename as input
+# and must add the results to it.
+
+# Retrieve the report file name
+report_filename = "sde_cox_ingersoll_ross_process.csv"
+if len(sys.argv) > 1:
+    report_filename = sys.argv[1]
+
+command = 'Rscript --vanilla ' + os.path.join(os.path.dirname(__file__), 'sde_cox_ingersoll_ross_process.r') + \
+          ' ' + report_filename
+
+# Block until command is finished
+subprocess.call(shlex.split(command))
