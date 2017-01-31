ba7c395a3445d738d109a1ead73fa10ae5642ea3
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2017-01-31 17:00:22 +0000

    #164 draft for ps1 using snakemake.

---
+++ sbpipe/tasks/ps1_analyse_data.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of sbpipe.
+#
+# sbpipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sbpipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sbpipe.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+# $Revision: 3.0 $
+# $Author: Piero Dalle Pezze $
+# $Date: 2016-11-01 15:43:32 $
+
+import os
+import sys
+import argparse
+import logging
+logger = logging.getLogger('sbpipe')
+
+SBPIPE = os.environ["SBPIPE"]
+sys.path.insert(0, SBPIPE)
+
+from sbpipe.tasks.utils import call_proc
+
+
+def ps1_analyse_data(model_name, scanned_par, inhibition_only, outputdir,
+        sim_data_folder, sim_plots_folder, repeat, percent_levels, min_level,
+        max_level, levels_number, homogeneous_lines, xaxis_label, yaxis_label):
+    """
+    Plot model single parameter scan time courses (Python wrapper).
+
+    :param model_name: the model name without extension
+    :param scanned_par: the model variable to scan
+    :param inhibition_only: true if the scanning only decreases the variable amount (inhibition only)
+    :param outputdir: the output directory
+    :param sim_data_folder: the name of the folder containing the simulated data
+    :param sim_plots_folder: the name of the folder containing the simulated plots
+    :param repeat: the simulation number
+    :param percent_levels: true if scanning levels are in percent
+    :param min_level: the minimum level
+    :param max_level: the maximum level
+    :param levels_number: the number of levels
+    :param homogeneous_lines: true if lines should be plotted homogeneously
+    :param xaxis_label: the label for the x axis (e.g. Time [min])
+    :param yaxis_label: the label for the y axis (e.g. Level [a.u.])
+    """
+
+    command = 'Rscript --vanilla ' + os.path.join(SBPIPE, 'sbpipe', 'R', 'sbpipe_ps1_main.r') + \
+        ' ' + model_name + ' ' + scanned_par + ' ' + inhibition_only + ' ' + outputdir + \
+        ' ' + sim_data_folder + ' ' + sim_plots_folder + ' ' + repeat + ' ' + percent_levels + ' ' + min_level + \
+        ' ' + max_level + ' ' + levels_number + ' ' + homogeneous_lines + ' ' + xaxis_label + ' ' + yaxis_label
+    call_proc(command)
+
+
+# this is a Python wrapper for ps1 analysis in R.
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model')
+    parser.add_argument('--scanned-par')
+    parser.add_argument('--inhibition-only')
+    parser.add_argument('--outputdir')
+    parser.add_argument('--sim-data-folder')
+    parser.add_argument('--sim-plot-folder')
+    parser.add_argument('--repeat', type=int, nargs='+')
+    parser.add_argument('--percent-levels')
+    parser.add_argument('--min-level')
+    parser.add_argument('--max-level')
+    parser.add_argument('--levels-number')
+    parser.add_argument('--homogeneous-lines')
+    parser.add_argument('--xaxis-label')
+    parser.add_argument('--yaxis-label')
+
+    args = parser.parse_args()
+    ps1_analyse_data(args.model, args.scanned_par, args.inhibition_only, args.outputdir, args.sim_data_folder, \
+        args.sim_plot_folder, args.repeat, args.percent_levels, args.min_level, args.max_level, args.levels_number, \
+        args.homogeneous_lines, args.xaxis_label, args.yaxis_label)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ sbpipe/tasks/ps1_postproc.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of sbpipe.
+#
+# sbpipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sbpipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sbpipe.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+# $Revision: 3.0 $
+# $Author: Piero Dalle Pezze $
+# $Date: 2016-11-01 15:43:32 $
+
+
+import os
+import sys
+import re
+from itertools import islice
+import shutil
+import argparse
+from sbpipe.tasks.utils import replace_str_in_report
+import logging
+logger = logging.getLogger('sbpipe')
+
+SBPIPE = os.environ["SBPIPE"]
+sys.path.insert(0, SBPIPE)
+
+
+def ps1_header_init(report, scanned_par):
+    """
+    Header report initialisation for single parameter scan pipeline.
+
+    :param report: a report
+    :param scanned_par: the scanned parameter
+
+    :return a list containing the header or an empty list if no header was created.
+    """
+
+    header = ['Time']
+    # Find the index of scanned_par in the header file, so it is possible to read the amount at
+    # the second line.
+    logger.debug("Retrieving column index for " + scanned_par +
+                 " from file " + report)
+    # Read the first line of a file.
+    with open(report) as myfile:
+        # 1 is the number of lines to read, 0 is the i-th element to extract from the list.
+        header = list(islice(myfile, 1))[0].replace('\n', '').split('\t')
+    logger.debug(header)
+    # Prepare the Header for the output files
+    # Add a \t at the end of each element of the header
+    header = [h + '\t' for h in header]
+    # Remove the \t for the last element.
+    header[-1] = header[-1].strip()
+    return header
+
+
+def generic_postproc(infile, outfile, scanned_par, simulate_intervals, single_param_scan_intervals):
+    """
+    Perform post processing organisation to single parameter scan report files.
+
+    :param infile: the model to process
+    :param outfile: the directory to store the results
+    :param scanned_par: the scanned parameter
+    :param simulate_intervals: the time step of each simulation
+    :param single_param_scan_intervals: the number of scans to perform
+    """
+
+    scanned_par_index = -1
+    scanned_par_level = -1
+    # Set the number of intervals
+    intervals = int(single_param_scan_intervals) + 1
+    # Set the number of timepoints
+    timepoints = int(simulate_intervals) + 1
+
+    shutil.copy(infile, outfile)
+
+    replace_str_in_report(outfile)
+
+    header = ps1_header_init(outfile, scanned_par)
+    if not header:
+        return
+
+    for j, name in enumerate(header):
+        # remove \n and \t from name
+        name = ''.join(name.split())
+        logger.debug(str(j) + " " + name + " " + scanned_par)
+        if name == scanned_par:
+            scanned_par_index = j
+            break
+    if scanned_par_index == -1:
+        logger.error("Column index for " + scanned_par + ": " + str(
+            scanned_par_index) + ". Species not found! You must add " + scanned_par +
+                     " to the Copasi report.")
+        return
+    else:
+        logger.debug("Column index for " + scanned_par + ": " + str(scanned_par_index))
+
+    logger.debug(outfile)
+
+    # Prepare the table content for the output files
+    for j in range(0, intervals):
+        # Read the scanned_par level
+        # Read the second line of a file.
+
+        with open(outfile, 'r') as myfile:
+            # 2 is the number of lines to read, 1 is the i-th element to extract from the list.
+            initial_configuration = list(islice(myfile, 2))[1].replace("\n", "").split('\t')
+            # print(initial_configuration)
+            scanned_par_level = initial_configuration[scanned_par_index]
+
+        if scanned_par_level == -1:
+            logger.error("scanned_par_level not configured!")
+            return
+        else:
+            logger.debug(
+                scanned_par + " level: " + str(scanned_par_level) + " (list index: " + str(
+                    scanned_par_index) + ")")
+
+        # copy the -th run to a new file: add 1 to timepoints because of the header.
+        round_scanned_par_level = scanned_par_level
+        # Read the first timepoints+1 lines of a file.
+        with open(outfile, 'r') as myfile:
+            table = list(islice(myfile, timepoints + 1))
+
+        # Write the extracted table to a separate file
+        with open(os.path.splitext(outfile)[0] + "__scan_" + scanned_par + "__level_" + str(
+                round_scanned_par_level) + ".csv", 'w') as myfile:
+            for line in table:
+                myfile.write(line)
+
+        with open(outfile, 'r') as myfile:
+            # read all lines
+            lines = myfile.readlines()
+
+        with open(outfile + "~", 'w') as myfile:
+            myfile.writelines(header)
+            myfile.writelines(lines[timepoints + 1:])
+
+        shutil.move(outfile + "~", outfile)
+
+
+def ps1_postproc(infile, outfile, scanned_par, simulate_intervals, single_param_scan_intervals, copasi=False):
+    """
+    Perform post processing organisation to single parameter scan report files.
+
+    :param infile: the model to process
+    :param outfile: the directory to store the results
+    :param scanned_par: the scanned parameter
+    :param simulate_intervals: the time step of each simulation
+    :param single_param_scan_intervals: the number of scans to perform
+    :param copasi: True if the model is a Copasi model
+    """
+    generic_postproc(infile, outfile, scanned_par, simulate_intervals, single_param_scan_intervals)
+    #if copasi:
+    #    copasi_postproc(infile, outfile, scanned_par, simulate_intervals, single_param_scan_intervals)
+    #else:
+    #    generic_postproc(infile, outfile, scanned_par, simulate_intervals, single_param_scan_intervals)
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-file')
+    parser.add_argument('-o', '--output-file')
+    parser.add_argument('-a', '--scanned-par')
+    parser.add_argument('-s', '--sim-intervals', default=1)
+    parser.add_argument('-p', '--ps1-intervals', default=1)
+    parser.add_argument('-c', '--copasi', action="store_true")
+    args = parser.parse_args()
+    ps1_postproc(args.input_file, args.output_file, args.scanned_par, int(args.sim_intervals), int(args.ps1_intervals), args.copasi)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
