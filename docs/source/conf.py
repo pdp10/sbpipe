c0611312ae5ca342dca262df6a876498be7ef7ab
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2017-02-03 09:28:31 +0000

    changed SB pipe to SBpipe (remaining files)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,7 +77,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'SB pipe'
+project = u'SBpipe'
 copyright = u'2016, Piero Dalle Pezze and Nicolas Le Novère'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -230,7 +230,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ('index', 'sbpipe.tex', u'SB pipe documentation',
+    ('index', 'sbpipe.tex', u'SBpipe documentation',
      u'Piero Dalle Pezze and Nicolas Le Novère', 'manual'),
 ]
 
@@ -260,7 +260,7 @@ latex_show_urls = True
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'sbpipe', u'SB pipe documentation',
+    ('index', 'sbpipe', u'SBpipe documentation',
      [u'Piero Dalle Pezze and Nicolas Le Novère'], 1)
 ]
 
@@ -274,7 +274,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'sbpipe', u'SB pipe documentation',
+    ('index', 'sbpipe', u'SBpipe documentation',
      u'Piero Dalle Pezze and Nicolas Le Novère', 'sbpipe', 'One line description of project.',
      'Miscellaneous'),
 ]
