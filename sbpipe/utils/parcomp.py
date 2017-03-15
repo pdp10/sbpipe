130e7e0c782d9ab3520cbd01031d9302760c021f
Piero Dalle Pezze <pdp10@users.noreply.github.com>
2017-03-15 11:27:06 +0000

    assignment

--- sbpipe/utils/parcomp.py
+++ sbpipe/utils/parcomp.py
@@ -40,7 +40,7 @@ def run_cmd(cmd):
     :param cmd: The string of the command to run
     """
     # This replacement is only needed for windows path
-    cmd.replace('\\', '\\\\')
+    cmd = cmd.replace('\\', '\\\\')
     logger.debug(cmd)
     if sys.version_info > (3,):
         with subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
@@ -58,7 +58,7 @@ def run_cmd_block(cmd):
     :param cmd: A tuple containing the string of the command to run
     """
     # This replacement is only needed for windows path
-    cmd.replace('\\', '\\\\')
+    cmd = cmd.replace('\\', '\\\\')
     logger.debug(cmd)    
     p = subprocess.call(shlex.split(cmd))
 
