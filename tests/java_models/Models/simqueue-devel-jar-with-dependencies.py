499e73ab8497fb9fbb2c530854c9d4f1f31f0212
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2017-04-04 15:23:24 +0100

    added test if Java is not installed.

--- tests/java_models/Models/simqueue-devel-jar-with-dependencies.py
+++ tests/java_models/Models/simqueue-devel-jar-with-dependencies.py
@@ -12,12 +12,19 @@ report_filename = "simqueue-devel-jar-with-dependencies.csv"
 if len(sys.argv) > 1:
     report_filename = sys.argv[1]
 
-# The Jar file for Java models includes a manifest.mf specifying the main class.
-command = 'java -jar ' + os.path.join(os.path.dirname(__file__), 'simqueue-devel-jar-with-dependencies.jar') + \
-          ' ' + report_filename
+try:
+    subprocess.Popen(['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
 
-# we replace \\ with / otherwise subprocess complains on windows systems.
-command = command.replace('\\', '\\\\')
+    # The Jar file for Java models includes a manifest.mf specifying the main class.
+    command = 'java -jar ' + os.path.join(os.path.dirname(__file__), 'simqueue-devel-jar-with-dependencies.jar') + \
+              ' ' + report_filename
 
-# Block until command is finished
-subprocess.call(shlex.split(command))
+    # we replace \\ with / otherwise subprocess complains on windows systems.
+    command = command.replace('\\', '\\\\')
+
+    # Block until command is finished
+    subprocess.call(shlex.split(command))
+
+
+except OSError as e:
+    print("Skipping test as no Java Virtual Machine was found.")
