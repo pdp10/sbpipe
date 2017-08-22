a685ae3481c6b807d278682023478c1b820f58ac
pdp10 <piero.dallepezze@gmail.com>
2017-08-22 10:55:05 +0000

    updated version.

--- install_deps_with_conda.sh
+++ install_deps_with_conda.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# This file is part of sbpipe.
+#
+# sbpipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sbpipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sbpipe.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+# $Revision: 1.0 $
+# $Author: Piero Dalle Pezze $
+# $Date: 2017-05-22 09:22:32 $
+
+
 
 ## clone sbpipe into working directory
 # git clone https://github.com/pdp10/sbpipe.git  path/to/workdir
