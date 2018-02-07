11175c5d2276509f6e3b393bb124c65381abf467
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2018-02-07 09:34:00 +0000

    #249 improved job name.

--- sbpipe/utils/parcomp.py
+++ sbpipe/utils/parcomp.py
@@ -229,8 +229,8 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
     if len(colnames) > 0:
         for column in colnames:
             # Now the same with qsub
-            jobs = "j" + column + cmd_iter_substr.strip('/') + "," + jobs
-            qsub_cmd = ["qsub", "-cwd", "-V", "-N", "j" + column + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + column), "-e", os.path.join(err_dir, "j" + column), "-b", "y", cmd.replace(cmd_iter_substr, column)]
+            jobs = "sbpipe" + column + "_" + cmd_iter_substr.strip('/') + "," + jobs
+            qsub_cmd = ["qsub", "-cwd", "-V", "-N", "sbpipe" + column + "_" + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + column), "-e", os.path.join(err_dir, "j" + column), "-b", "y", cmd.replace(cmd_iter_substr, column)]
             logger.debug(qsub_cmd)
             logger.info('Starting Task ' + column)
             if sys.version_info > (3,):
@@ -242,8 +242,8 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
     else:
         for i in range(1, runs + 1):
             # Now the same with qsub
-            jobs = "j" + str(i) + cmd_iter_substr.strip('/') + "," + jobs
-            qsub_cmd = ["qsub", "-cwd", "-V", "-N", "j" + str(i) + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + str(i)), "-e", os.path.join(err_dir, "j" + str(i)), "-b", "y", cmd.replace(cmd_iter_substr, str(i))]
+            jobs = "sbpipe" + str(i) + "_" + cmd_iter_substr.strip('/') + "," + jobs
+            qsub_cmd = ["qsub", "-cwd", "-V", "-N", "sbpipe" + str(i) + "_" + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + str(i)), "-e", os.path.join(err_dir, "j" + str(i)), "-b", "y", cmd.replace(cmd_iter_substr, str(i))]
             logger.debug(qsub_cmd)
             logger.info('Starting Task ' + str(i))
             if sys.version_info > (3,):
@@ -254,7 +254,7 @@ def run_jobs_sge(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
                 qsub_proc.communicate()[0]
     # Check here when these jobs are finished before proceeding
     # don't add names for output and error files as they can generate errors..
-    qsub_cmd = ["qsub", "-sync", "y", "-b", "y", "-o", "/dev/null", "-e", "/dev/null", "-hold_jid", jobs[:-1], "sleep", "1"]
+    qsub_cmd = ["qsub", "-sync", "y", "-b", "y", "-o", "/dev/null", "-e", "/dev/null", "-hold_jid", jobs[:-1], "sbpipeW_" + cmd_iter_substr.strip('/'), "1"]
     if sys.version_info > (3,):
         with subprocess.Popen(qsub_cmd, stdout=subprocess.PIPE) as p:
             p.communicate()[0]
@@ -283,8 +283,8 @@ def run_jobs_lsf(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
     if len(colnames) > 0:
         for column in colnames:
             for i in range(1, runs + 1):
-                jobs = "done(j" + column + cmd_iter_substr.strip('/') + ")&&" + jobs
-                bsub_cmd = ["bsub", "-cwd", "-J", "j" + column + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + column), "-e",
+                jobs = "done(sbpipe" + column + "_" + cmd_iter_substr.strip('/') + ")&&" + jobs
+                bsub_cmd = ["bsub", "-cwd", "-J", "sbpipe" + column + "_" + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + column), "-e",
                             os.path.join(err_dir, "j" + column), cmd.replace(cmd_iter_substr, column)]
                 logger.debug(bsub_cmd)
                 logger.info('Starting Task ' + column)
@@ -296,8 +296,8 @@ def run_jobs_lsf(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
                     bsub_proc.communicate()[0]
     else:
         for i in range(1, runs + 1):
-            jobs = "done(j" + str(i) + cmd_iter_substr.strip('/') + ")&&" + jobs
-            bsub_cmd = ["bsub", "-cwd", "-J", "j" + str(i) + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + str(i)), "-e", os.path.join(err_dir, "j" + str(i)), cmd.replace(cmd_iter_substr, str(i))]
+            jobs = "done(sbpipe" + str(i) + "_" + cmd_iter_substr.strip('/') + ")&&" + jobs
+            bsub_cmd = ["bsub", "-cwd", "-J", "sbpipe" + str(i) + "_" + cmd_iter_substr.strip('/'), "-o", os.path.join(out_dir, "j" + str(i)), "-e", os.path.join(err_dir, "j" + str(i)), cmd.replace(cmd_iter_substr, str(i))]
             logger.debug(bsub_cmd)
             logger.info('Starting Task ' + str(i))
             if sys.version_info > (3,):
@@ -310,7 +310,7 @@ def run_jobs_lsf(cmd, cmd_iter_substr, out_dir, err_dir, runs=1, colnames=[]):
     import random
     import string
     job_name = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(7))
-    bsub_cmd = ["bsub", "-J", job_name, "-o", "/dev/null", "-e", "/dev/null", "-w", jobs[:-2], "sleep", "1"]
+    bsub_cmd = ["bsub", "-J", job_name, "-o", "/dev/null", "-e", "/dev/null", "-w", jobs[:-2], "sbpipeW_" + cmd_iter_substr.strip('/'), "1"]
     logger.debug(bsub_cmd)
     if sys.version_info > (3,):
         with subprocess.Popen(bsub_cmd, stdout=subprocess.PIPE) as p:
